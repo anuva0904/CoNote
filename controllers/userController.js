70e49b6d27e2d87e79cbe8ddcd5bd52aafa28649
Anuva Singh <singhanuva4@gmail.com>
2025-10-24 17:14:45 +0530

    Create User registrations func

---
+++ controllers/userController.js
@@ -0,0 +1,60 @@
+const jwt=require('jsonwebtoken');
+const User=require('./models/usermodel');
+const {generateToken} =('./utils/')
+const bcrypt=require('bcrypt');
+require('dotenv').config();
+
+
+//User Registration 
+exports.registerUser=async(req,res)=>{
+    const {username,email,password,role,profilePic}=req.body;
+    try{
+        if(!username || !email || !password){
+            return res.status(400).json({message:'please provide all required fields'});
+        }
+
+        const existingUser=await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message:'User already exists'});
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        const newUser = new User({
+            name: username,
+            email,
+            password: hashedPassword,
+            role,
+            profilePic
+        });
+
+
+        await newUser.save();
+
+        const jwttoken= generateToken({id: newUser._id});
+        res.cookie('token',jwttoken,{httpOnly:true, maxAge:7*24*60*60*1000});
+
+        return res.redirect('/login');
+
+    }catch(err){
+        console.error('Error registering user:', err.message);
+        return res.render('register', {error: 'Server error. Please try again later.'});
+    }
+}
+
+//User Login 
+
+
+
+
+//User Logout 
+
+
+//User Delete 
+
+
+//get user profile 
+
+
+
+//Update User
---
+++ index.js
@@ -0,0 +1,20 @@
+//Global modules
+const express =require('express');
+const mongoose=require('mongoose');
+require('dotenv').config();
+const app=express();
+
+
+
+//Local modules
+const db=require('./config/dbconnection');
+db();
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+const port=process.env.PORT;
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
