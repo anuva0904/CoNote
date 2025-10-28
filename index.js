7a8ad802f2e9a31dcee11438e5cb540544d0b70c
Anuva Singh <singhanuva4@gmail.com>
2025-10-28 21:54:49 +0530

    app file created

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
 //Global modules
 const express =require('express');
 const mongoose=require('mongoose');
+const cookieParser = require('cookie-parser');
+const path =require('path');
 require('dotenv').config();
-const app=express();
-
-
-
-
 
 //Local modules
 const db=require('./config/dbconnection');
+const userRoutes = require('./routers/userRouters');
+const notesRoutes = require('./routers/notesRouter');
+const folderRoutes = require('./routers/folderRouter');
+
+//Initializing app
+const app=express();
+
+//Setting up db connection
 db();
 
+//Middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-})
+//Setting view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-const port=process.env.PORT;
+// Routes
+app.use('/users', userRoutes);
+app.use('/notes', notesRoutes);
+app.use('/folders', folderRoutes);
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).render('404', { message: 'Page not found' });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).render('500', { message: 'Something went wrong!' });
+});
+
+const port=process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
-})
+});
